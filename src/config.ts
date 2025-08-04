c77fb146df03b1536d8fe594c4763b589dde72bb
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-08-04 16:13:28 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Afonso Beato';
-export const SITE_DESCRIPTION = 'This is my corner on the internet. I am a student and a CS enthusiast.';
+export const SITE_TITLE = 'fonz beato';
+export const SITE_DESCRIPTION = 'my little corner on the web, i think?';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
