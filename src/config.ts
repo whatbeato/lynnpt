132bc8eb97ee4c1f3d42d762bdf2604419c3ccfe
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-08-25 10:20:03 +0000

    dark theme, remove embed image :D

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'fonz beato';
+export const SITE_TITLE = 'lynn beato';
 export const SITE_DESCRIPTION = 'my little corner on the web, i think?';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
